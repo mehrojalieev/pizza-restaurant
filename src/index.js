0075045631bf92c949fbda05cb10c784fef589cd
mehrojalieev <aliyevmehrojbek0@gmail.com>
2023-11-13 22:49:00 +0500

    Added loading

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
+import { Suspense, lazy } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import { BrowserRouter } from 'react-router-dom'
+const App = lazy(() => import("./App"))
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
+    <Suspense fallback={<p>Loading...</p>}>
     <App />
+    </Suspense>
   </BrowserRouter>
 );
 
