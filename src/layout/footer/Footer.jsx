27541cc34bc28a25cc97ed799f2b66dd307ecf40
mehrojalieev <aliyevmehrojbek0@gmail.com>
2023-11-30 04:40:54 +0500

    Added Copyright

--- src/layout/footer/Footer.jsx
+++ src/layout/footer/Footer.jsx
@@ -5,36 +5,42 @@ import { Link } from "react-router-dom";
 const Footer = () => {
     return (
         <footer>
-            <div className="footer__content">
-                <Link className="footer__logo">FOODMOOD</Link>
-                <p>Vivamus convallis fermen sollicitudin et, vitae elit in vel ultrices sed in urna ipsum ullamcorper auctor.</p>
-                <div className="social-icons">
-                    <i><AiOutlineTwitter /></i>
-                    <i><FaFacebookF /></i>
-                    <i><AiOutlineInstagram /></i>
+            <div className="footer__content-container">
+                <div className="footer__content">
+                    <Link className="footer__logo">FOODMOOD</Link>
+                    <p>Vivamus convallis fermen sollicitudin et, vitae elit in vel ultrices sed in urna ipsum ullamcorper auctor.</p>
+                    <div className="social-icons">
+                        <i><AiOutlineTwitter /></i>
+                        <i><FaFacebookF /></i>
+                        <i><AiOutlineInstagram /></i>
+                    </div>
                 </div>
+                <ul className="footer__menu">
+                    <li ><Link className="menu-title">ABOUT US</Link></li>
+                    <li><Link>Our Story</Link></li>
+                    <li><Link>Our Locations</Link></li>
+                    <li><Link>Current Deals</Link></li>
+                    <li><Link to="/contact">Contact Us</Link></li>
+                </ul>
+                <ul className="footer__menu">
+                    <li><Link className="menu-title">OUR MENU</Link></li>
+                    <li><Link>Pizza</Link></li>
+                    <li><Link>Pasta</Link></li>
+                    <li><Link>Desert</Link></li>
+                    <li><Link>Take & Bake</Link></li>
+                </ul>
+                <ul className="footer__menu">
+                    <li><Link className="menu-title">OUR LOCATION</Link></li>
+                    <li><Link>CHILONZOR</Link></li>
+                    <li><Link>CHIRCHIQ</Link></li>
+                    <li><Link>UCH TEPA</Link></li>
+                    <li><Link>QO'YLIQ</Link></li>
+                </ul>
             </div>
-            <ul className="footer__menu">
-                <li ><Link className="menu-title">ABOUT US</Link></li>
-                <li><Link>Our Story</Link></li>
-                <li><Link>Our Locations</Link></li>
-                <li><Link>Current Deals</Link></li>
-                <li><Link to="/contact">Contact Us</Link></li>
-            </ul>
-            <ul className="footer__menu">
-                <li><Link className="menu-title">OUR MENU</Link></li>
-                <li><Link>Pizza</Link></li>
-                <li><Link>Pasta</Link></li>
-                <li><Link>Desert</Link></li>
-                <li><Link>Take & Bake</Link></li>
-            </ul>
-            <ul className="footer__menu">
-                <li><Link className="menu-title">OUR LOCATION</Link></li>
-                <li><Link>CHILONZOR</Link></li>
-                <li><Link>CHIRCHIQ</Link></li>
-                <li><Link>UCH TEPA</Link></li>
-                <li><Link>QO'YLIQ</Link></li>
-            </ul>
+            <div className="footer__copyright">
+                <p>Copyright © 2022 The Pizzeria</p>
+                <p>Powered by The FOODMOOD</p>
+            </div>
         </footer>
     )
 }
