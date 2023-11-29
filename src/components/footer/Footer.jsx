f087554b6f03a3047e260f341680024b8fccacb6
mehrojalieev <aliyevmehrojbek0@gmail.com>
2023-11-29 20:23:46 +0500

    Footer finished

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -14,22 +14,22 @@ const Footer = () => {
                     <i><AiOutlineInstagram /></i>
                 </div>
             </div>
-            <ul>
-                <li><Link>ABOUT US</Link></li>
+            <ul className="footer__menu">
+                <li ><Link className="menu-title">ABOUT US</Link></li>
                 <li><Link>Our Story</Link></li>
                 <li><Link>Our Locations</Link></li>
                 <li><Link>Current Deals</Link></li>
                 <li><Link to="/contact">Contact Us</Link></li>
             </ul>
-            <ul>
-                <li><Link>OUR MENU</Link></li>
+            <ul className="footer__menu">
+                <li><Link className="menu-title">OUR MENU</Link></li>
                 <li><Link>Pizza</Link></li>
                 <li><Link>Pasta</Link></li>
                 <li><Link>Desert</Link></li>
                 <li><Link>Take & Bake</Link></li>
             </ul>
-            <ul>
-                <li><Link>OUR LOCATION</Link></li>
+            <ul className="footer__menu">
+                <li><Link className="menu-title">OUR LOCATION</Link></li>
                 <li><Link>CHILONZOR</Link></li>
                 <li><Link>CHIRCHIQ</Link></li>
                 <li><Link>UCH TEPA</Link></li>
