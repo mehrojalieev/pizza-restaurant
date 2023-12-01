bdeaec3da5678d8c78b336959153d00e6041c373
mehrojalieev <aliyevmehrojbek0@gmail.com>
2023-12-01 20:17:50 +0500

    Created Outles

---
+++ src/components/outles/Outles.jsx
@@ -0,0 +1,14 @@
+import "./Outles.scss"
+
+const Outles = () => {
+  return (
+    <div className="outles">
+        <div className="outles-title">
+            <strong>OUR OUTLES</strong>
+            <h2>Find an Outlet Near You</h2>
+        </div>
+    </div>
+  )
+}
+
+export default Outles
