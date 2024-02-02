586e5afcaf85e07d6b04980444b39437dcfe4dd6
aliyev_mehrojbek <aliyevmehrojbek0@gmail.com>
2024-02-02 23:53:56 +0500

    Created container

--- src/layout/nav/Nav.jsx
+++ src/layout/nav/Nav.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 import { Link, NavLink, useLocation } from 'react-router-dom'
 import Logo from '../../assets/Images/logofood.png'
 import Home from '../../pages/home/Home';
+import { Container } from '../../utils/Utils';
 
 const Nav = () => {
 
@@ -10,40 +11,42 @@ const Nav = () => {
 
   const { pathname } = useLocation()
   return pathname.includes("/signup") || pathname.includes("/login") || pathname.includes("/cabinet") ? null : (
-    <>
-      <nav>
-        <img src={Logo} alt="Logo" />
+    <nav>
+      <Container>
+        <div className="nav-wrapper">
+          <img src={Logo} alt="Logo" />
 
-        <div className="nav-menu">
-          <ul>
-            <li>
-              <NavLink className={({ isActive }) => isActive ? "active" : "pending"} to='/'>Home</NavLink>
-            </li>
-            <li>
-              <NavLink className={({ isActive }) => isActive ? "active" : "pending"} to='menu'>Our Menu</NavLink>
-            </li>
-            <li>
-              <NavLink to='offer'>Offer</NavLink>
-            </li>
-            <li>
-              <NavLink to='about'>About</NavLink>
-            </li>
-            <li>
-              <NavLink to='contact'>Contact</NavLink>
-            </li>
-          </ul>
+          <div className="nav-menu">
+            <ul>
+              <li>
+                <NavLink className={({ isActive }) => isActive ? "active" : "pending"} to='/'>Home</NavLink>
+              </li>
+              <li>
+                <NavLink className={({ isActive }) => isActive ? "active" : "pending"} to='menu'>Our Menu</NavLink>
+              </li>
+              <li>
+                <NavLink to='offer'>Offer</NavLink>
+              </li>
+              <li>
+                <NavLink to='about'>About</NavLink>
+              </li>
+              <li>
+                <NavLink to='contact'>Contact</NavLink>
+              </li>
+            </ul>
+          </div>
+          <div className="nav-register">
+            {
+              checkToken ? <Link to="/cabinet">Account</Link> : <>
+                <NavLink className={'register-btn'} to='signup'>Sign up</NavLink>
+                <NavLink className={'register-btn'} to='login'>Login</NavLink>
+              </>
+            }
+          </div>
         </div>
-        <div className="nav-register">
-          {
-            checkToken ? <Link to="/cabinet">Account</Link> : <>
-              <NavLink className={'register-btn'} to='signup'>Sign up</NavLink>
-              <NavLink className={'register-btn'} to='login'>Login</NavLink>
-            </>
-          }
-        </div>
-      </nav>
+      </Container>
+    </nav>
 
-    </>
   )
 }
 
---
+++ src/utils/Utils.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import "./Utils.scss"
+const Utils = () => {
+  return (
+    <div>Utils</div>
+  )
+}
+
+const Container = ({children}) => {
+    return (
+        <div className="container">
+                {children}
+        </div>
+    )
+}
+
+export  {Utils, Container}
