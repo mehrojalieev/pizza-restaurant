8b50a6cf491d23fe3480d38c8da4f491e97e5fd9
“mehrojalieev” <aliyevmehrojbek8006@gmail.com>
2024-03-08 08:29:06 +0500

    Finished Responsive of Features

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -13,7 +13,7 @@ const Home = () => {
     <>
       <Hero />
       {/* <Category /> */}
-      {/* <Features/> */}
+      <Features/>
       {/* <Outles/> */}
       {/* <Social/> */}
       {/* <Footer /> */}
